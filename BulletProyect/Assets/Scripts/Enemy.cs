1b12f40b4b87dad705f63ee682a4504604fd504e
Miguel V <migueldvp2001@outlook.com>
2023-04-05 11:09:51 -0500

    Movimiento de enemigo
    El movimiento del enemigo hacia el jugador creo que ya esta bien

--- BulletProyect/Assets/Scripts/Enemy.cs
+++ BulletProyect/Assets/Scripts/Enemy.cs
@@ -6,7 +6,8 @@ public class Enemy : MonoBehaviour
 {
     public GameObject player;
     float distance;
-    private float speed = 4;
+    private float speed = 5;
+    private float speedr;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,7 @@ public class Enemy : MonoBehaviour
         distance = Vector3.Distance(transform.position, player.transform.position);
         if (distance <= 6)
         {
+            speedr = 75;
             // Obtener la dirección hacia el jugador
             Vector3 direction = player.transform.position - transform.position;
             direction.z = 0; // Establecer la dirección en el plano XY
@@ -27,11 +29,15 @@ public class Enemy : MonoBehaviour
             // Calcular el ángulo de rotación en el eje Z
             float rotationZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-            // Establecer la rotación del enemigo hacia el jugador
-            transform.rotation = Quaternion.Euler(0, 0, rotationZ - 90);
+            // Calcular la rotación deseada
+            Quaternion desiredRotation = Quaternion.Euler(0, 0, rotationZ - 90);
+
+            // Aplicar la rotación a una velocidad constante
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRotation, speedr * Time.deltaTime);
         }
         if (distance > 6 && distance <= 14f)
         {
+            speedr = 140;
             // Obtener la dirección hacia el jugador
             Vector3 direction = player.transform.position - transform.position;
             direction.z = 0; // Establecer la dirección en el plano XY
@@ -39,11 +45,14 @@ public class Enemy : MonoBehaviour
             // Calcular el ángulo de rotación en el eje Z
             float rotationZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-            // Establecer la rotación del enemigo hacia el jugador
-            transform.rotation = Quaternion.Euler(0, 0, rotationZ - 90);
+            // Calcular la rotación deseada
+            Quaternion desiredRotation = Quaternion.Euler(0, 0, rotationZ - 90);
+
+            // Aplicar la rotación a una velocidad constante
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRotation, speedr * Time.deltaTime);
 
             // Mover hacia el jugador
-            transform.Translate(speed * Time.deltaTime, 0, 0);
+            transform.Translate(0, speed * Time.deltaTime, 0);
         }
     }
 }
